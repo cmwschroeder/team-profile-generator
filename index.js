baf7ad59ccec8b9cffa821976deceee354797763
cmwschroeder <cmwschroeder@gmail.com>
2022-04-21 13:42:43 -0700

    questions to manager have been added, are now asked with inquirer, nothing done with answers yet

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-//Questions to ask the user from inquirer
-const questions = [];
+//Questions to ask the user from inquirer, specifically for the very first team memeber, the manager.
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'managerName',
+        message: "Enter team manager's name: ",
+      },
+      {
+        type: 'input',
+        name: 'managerId',
+        message: "Enter team manager's id: ",
+      },
+      {
+        type: 'input',
+        name: 'managerEmail',
+        message: "Enter team manager's email: ",
+      },
+      {
+        type: 'input',
+        name: 'managerOfficeNumber',
+        message: "Enter team manager's office number: ",
+      },
+      {
+        type: 'list',
+        name: 'next',
+        message: 'Would you like to: a',
+        choices: ['Add an Engineer', 'Add an Intern', 'Finish building team'],
+      },
+];
 
 // This will hold all of the employees
 const team = [];
@@ -77,3 +104,18 @@ function Intern(name, id, email, school) {
         return this.school;
     }
 }
+
+function init() {
+    inquirer
+    .prompt(managerQuestions).then((answers) => {
+        console.log(answers);
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+            console.log("Error with inputs")
+          }
+    })
+}
+
+init();
